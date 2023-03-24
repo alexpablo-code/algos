2b8f8575e04194c96881f91edbaa0f997619778c
Rony Pablo <ronyalexis0@gmail.com>
2023-03-24 11:10:31 -0700

    front assignment

---
+++ fronts.js
@@ -0,0 +1,57 @@
+class Node{
+    constructor(data){
+        this.data = data
+        this.next = null
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null
+        // this.tail = null
+        // this.length = 0
+    }
+    
+    addFront(value){
+        let newNode = new Node(value);
+    
+        //if the list is empty
+        if(!this.head){
+            this.head = newNode;
+            return this;
+        }
+    
+        newNode.next = this.head;
+        this.head = newNode;
+        return this;
+    }
+
+    removeFront(){
+
+        if(!this.head){
+            return null
+        }
+        else{
+            this.head = this.head.next;
+            return this;
+        }
+    }
+
+    frontData(){
+        if(!this.head){
+            return null;
+        }
+        console.log(this.head.data);
+        return this;
+    }
+
+
+}
+
+const SLL = new SinglyLinkedList();
+SLL.addFront('A').addFront('b').addFront('c');
+console.log(SLL);
+SLL.removeFront();
+console.log(SLL)
+SLL.frontData()
+
