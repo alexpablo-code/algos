f1b5a81ce78393bb00dce16dee73b9b3f1d28987
Rony Pablo <ronyalexis0@gmail.com>
2023-04-07 14:50:03 -0700

    insert to Linked list

--- ztm_singly_linked_list.js
+++ ztm_singly_linked_list.js
@@ -1,20 +1,24 @@
-//10 --> 5 --> 16
 
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
 
 class Linkedlist{
-    constructor(value){
-        this.head = {
-            value: value,
-            next: null
-        }
-        this.tail = this.head;
-        this.length = 1;
+    constructor(){
+        this.head = null
+        this.tail = null;
+        this.length = 0;
     }
 
     append(value){
-        const newNode = {
-            value: value,
-            next: null
+        const newNode = new Node(value);
+        if(this.head == null){
+            this.head = newNode;
+            this.tail = newNode
+            return this;
         }
         this.tail.next = newNode;
         this.tail = newNode;
@@ -23,17 +27,47 @@ class Linkedlist{
     }
 
     prepend(value){
-        const newNode = {
-            value: value,
-            next: this.head
-        }
+        const newNode = new Node(value);
+        newNode.next = this.head;
         this.head = newNode
         this.length++;
         return this;
     }
+
+    printList(){
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    insert(idx, value){
+        const newNode = new Node(value);
+        let currentNode = this.head;
+        let nextIdx = 0
+        while(currentNode !== null){
+            nextIdx++;
+            if(idx === 0){
+                this.prepend(value);
+                return this;
+            }
+            else if(nextIdx == idx){
+                newNode.next = currentNode.next;
+                currentNode.next = newNode;
+                return this;
+            }
+            currentNode = currentNode.next;
+        }
+        return this;
+    }
 }
 
-const myLinkedList = new Linkedlist(10);
+const myLinkedList = new Linkedlist();
 myLinkedList.append(5);
 myLinkedList.prepend(1)
-console.log(myLinkedList);
+console.log(myLinkedList.printList());
+myLinkedList.insert(1,8);
+console.log(myLinkedList.printList());
