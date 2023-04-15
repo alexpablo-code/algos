64f8ff9ac0ab7d3e950b6e451631ebe7c87652bd
Rony Pablo <ronyalexis0@gmail.com>
2023-04-15 10:28:45 -0700

    stack with array

---
+++ ztm_stack_array.js
@@ -0,0 +1,25 @@
+
+
+class Stack {
+    constructor(){
+        this.array = [];
+    }
+
+    peek(){
+        return this.array[this.array.lenght - 1];
+    }
+
+    push(value){
+        this.array.push(value);
+        return this;
+    }
+
+    pop(){
+        this.array.pop();
+        return this;
+    }
+}
+
+const newStack = new Stack();
+
+console.log(newStack.push('Google'));
