9d8df48a95be65c8bd11ce2f65bdfbdbef52eedc
Rony Pablo <ronyalexis0@gmail.com>
2023-04-16 20:56:13 -0700

    find target sum using hash, fixed

--- algos_practice.js
+++ algos_practice.js
@@ -23,8 +23,43 @@ function difference(arr){
     
 }
 
-console.log(difference([
-    [11,2,4],
-    [4,5,6],
-    [10,8,-12]
-]))
+function twoNumberSum(array, targetSum) {
+    // Write your code here.
+    let sumArr = [];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] == targetSum) {
+                sumArr.push(array[i]);
+                sumArr.push(array[j]);
+                return sumArr;
+            }
+        }
+    }
+    return sumArr;
+}
+function twoNumberSumHash(array, targetSum) {
+    // Write your code here.
+    const resultNums = {};
+    let sumArr=[];
+    for(let i = 0; i < array.length; i++){
+        let currentNum = array[i];
+        let otherNum = targetSum - array[i];
+        console.log(otherNum);
+        if(otherNum in resultNums){
+            console.log('found other');
+            sumArr.push(array[i]);
+            sumArr.push(otherNum);
+            return sumArr;
+        }
+        resultNums[currentNum]=true;
+    }
+    
+    return resultNums;
+}
+
+// console.log(difference([
+//     [11,2,4],
+//     [4,5,6],
+//     [10,8,-12]
+// ]))
+console.log(twoNumberSumHash([3, 5, -4, 8, 11, 1, -1, 6], 10));
