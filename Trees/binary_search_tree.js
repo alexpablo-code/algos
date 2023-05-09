3990bf845b8b31273735094e233f02853f7efb8d
Rony Pablo <ronyalexis0@gmail.com>
2023-05-09 08:58:46 -0700

    insert and lookup binary tree

--- Trees/binary_search_tree.js
+++ Trees/binary_search_tree.js
@@ -15,26 +15,62 @@ class BinarySearchTree{
         const newNode = new Node(value);
         if(this.root == null){
             this.root = newNode;
-        }
-        currentNode = this.root;
-        while(currentNode !== null){
-            if(newNode.value > currentNode.value){
-                currentNode = currentNode.right;
-            } else{
-                currentNode = currentNode.left
+        } else{
+            let currentNode = this.root;
+            while(true){
+                if(value < currentNode.value){
+                    //go left
+                    if(!currentNode.left){
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    // go right
+                    if(!currentNode.right){
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
             }
-        
+        }
         }
 
+        lookup(value){
+            if(!this.root){
+                return false;
+            }
+            let currentNode = this.root;
+            while(currentNode){
+                if(value < currentNode.value){
+                    currentNode = currentNode.left;
+                } else if(value > currentNode.value){
+                    currentNode = currentNode.right;
+                } else if(value == currentNode.value){
+                    return currentNode;
+                }
+            }
+            return false;
+        }
     }
-
-    lookup(){
-
-    }
-}
 
 const newTree = new BinarySearchTree();
+newTree.insert(9);
+newTree.insert(4);
+newTree.insert(6);
 
+console.log(newTree.lookup(4));
+// console.log(JSON.stringify(traverse(newTree.root)));
 //      9   
 //   4     20  
 //  1 6   15 170
+
+function traverse(node){
+    const tree = {value: node.value};
+    tree.left = node.left === null ? null:
+    traverse(node.left);
+    tree.right = node.right ===null ? null:
+    traverse(node.right);
+    return tree;
+}
